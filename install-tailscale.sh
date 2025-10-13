bde7adbf78956aeba8a7468744c5498b3e2a48bd
Gavin Fancher <gavin@mac.local>
2025-10-13 13:26:40 -0500

    updates tailscale install for automation

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -4,4 +4,8 @@ curl -fsSL https://tailscale.com/install.sh | sh
 
 sudo tailscale set --operator="$USER"
 
-tailscale up
+read -p "enter linux setup key: " TS_KEY
+
+sudo tailscale up --authkey="$TS_KEY"
+
+echo "success!"
