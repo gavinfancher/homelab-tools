75dd650a3fd7fc5ba424b338e4d8e785ec1db6d5
Gavin Fancher <gavin@Mac.localdomain>
2025-10-12 20:19:41 -0500

    minor update to clone tool

--- clone-999-vm.sh
+++ clone-999-vm.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-read -p "source of clone: " clone_source
-read -p "clone number: " clone_number
-read -p "clone name: " clone_name
-read -p "clone disk location: " clone_location
-
-read -p "clone cores: " clone_cores
-read -p "clone memory (in GB): " clone_mem_in_GB
-
-clone_mem_in_MB=$((clone_mem_in_GB * 1024))
-
-qm clone "$clone_source" "$clone_number" --name "$clone_name" --full --storage "$clone_location"
-qm set "$clone_number" --cores "$clone_cores" --memory "$clone_mem_in_MB"
-
-#!/bin/bash
-
 default_source=999
 defualt_storage="samsung-2tb-a"
 
